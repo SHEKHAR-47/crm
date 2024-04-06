b46f44a8e8f78a4199d52eb16b6d4039504fc36a
Shekhar-47 <shekharpadhy123@gmail.com>
2024-04-06 08:50:36 +0530

    positioning change at leftbar

--- admin/leftbar.php
+++ admin/leftbar.php
@@ -18,9 +18,9 @@
     <ul>	
       <li class="start"> <a href="home.php"> <i class="icon-custom-home"></i> <span class="title">Dashboard</span>  </a> 
 		    </li>
-      <li><a href="manage-tickets.php"><span class="fa fa-ticket"></span> Manage Ticket</a></li>
-      <li ><a href="manage-quotes.php"> <span class="fa fa-tasks"></span> Manage Quotes</a></li> 
-      <li><a href="user-access-log.php"><span class="fa fa-users"></span>&nbsp;&nbsp;User Access Log</a></li>
-      <li><a href="manage-users.php"><span class="fa fa-users"></span> Users</a></li>
-      <li><a href="change-password.php"><span class="fa fa-file-text-o"></span> Change Password</a></li>
+        <li ><a href="manage-quotes.php"> <span class="fa fa-tasks"></span> Manage Quotes</a></li> 
+        <li><a href="user-access-log.php"><span class="fa fa-users"></span>&nbsp;&nbsp;Customer Access Log</a></li>
+        <li><a href="manage-users.php"><span class="fa fa-users"></span> Customers</a></li>
+        <li><a href="manage-tickets.php"><span class="fa fa-ticket"></span> Manage Ticket</a></li>
+        <li><a href="change-password.php"><span class="fa fa-file-text-o"></span> Change Password</a></li>
     </ul>
